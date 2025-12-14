aaa733d23c61d6ddbd9602c93910b3dd5dce27f1
Jody Ribton <jody@ribton.me>
2025-12-14 10:48:13 +0000

    Basic schematic and first version of source.

---
+++ src/therm.c
@@ -0,0 +1,42 @@
+#define F_CPU 1000000UL
+
+#include <avr/io.h>                   
+#include <util/delay.h>
+
+// ADC1 on PB2
+// Clock on PB3
+// Reset on PB4
+
+void show_temp(uint8_t t) {
+	// Trigger reset of 7-segment displays
+	PORTB |= (1<<PB4);
+	PORTB &= ~(1<<PB4);
+
+	// Clock the displays up to the temperature value.
+	for (uint8_t i = 0; i<t; i++) {
+		PORTB |= (1<<PB3);
+		PORTB &= ~(1<<PB3);
+	}
+}
+
+int main(void) {
+	DDRB = (1<<DDB3) | (1<<DDB4);
+
+	ADMUX |= (1<<REFS2) | (1<<REFS1); // 2.56v ref, no cap
+	ADMUX |= (1<<MUX0); // ADC on PB2 / ADC1
+	ADMUX |= (1<<ADLAR); // Left shift to ADCH only
+	ADCSRA = (1<<ADEN) | (1<<ADPS1) | (1<<ADPS0); // Prescaler 8, ADC enabled
+
+	while (1) {
+		// Read temperature from ADC
+		ADCSRA |= (1<<ADSC);
+		while (ADCSRA & (1<<ADSC));
+
+		uint8_t adc_read = ADCH; // 8 bit read, 1 = 10mV
+		uint8_t temperature = adc_read-50; // 500mV = 0 deg C
+		show_temp(temperature);
+
+		// Read and update temperature every 30 seconds.
+		_delay_ms(30000);
+	}
+}
